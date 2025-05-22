b22235887e471cc34c1529785d023602c45b569c
adantuon <adantuon@mail.umw.edu>
2025-05-22 17:58:06 -0400

    Add project files.

---
+++ CPSC440_Lab3.cpp
@@ -0,0 +1,9 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5//allegro_primitives.h>
+#include <allegro5/allegro_native_dialog.h>
+
+int main()
+{
+}
